a97a6426133a9bd24a45d98bbffed729e0014c8f
MathWithJerome <89851058+MathWithJerome@users.noreply.github.com>
2022-09-06 11:39:41 -0400

    new data source

--- bestball/bestball.R
+++ bestball/bestball.R
@@ -8,16 +8,16 @@ library(ompr.roi)
 library(tidyr)
 library(tibble)
 
-players.l = list()
-for (y in 2010:2020) {
+bestball.l = list()
+for (y in 2010:2021) {
   print(y)
-  player.pool = fd.w %>% filter(season == y, Pos %in% c("QB","RB","WR","TE")) %>% 
-    group_by(Player) %>% 
-    summarise(total.pts = sum(HalfPPRFantasyPoints)) %>% 
+  player.pool = fp.w %>% filter(season == y, position %in% c("QB","RB","WR","TE")) %>% 
+    group_by(full_name) %>% 
+    summarise(total.pts = sum(half_ppr)) %>% 
     filter(total.pts > 50) %>% 
-    select(Player)
+    select(full_name)
   
-  # pts = fd.w %>% filter(season == y, Pos %in% c("QB","RB","WR","TE"), Player %in% player.pool$Player) %>%
+  # pts = fp.w %>% filter(season == y, Pos %in% c("QB","RB","WR","TE"), Player %in% player.pool$Player) %>%
   #   select(Player, Pos, Tm, week, HalfPPRFantasyPoints) %>%
   #   mutate(is.flex = if_else(Pos %in% c("RB","WR","TE"), T, F))
   # 
@@ -25,53 +25,58 @@ for (y in 2010:2020) {
   #   replace(is.na(.), 0) %>%
   #   column_to_rownames(var = "Player")
   
-  points = fd.w %>% filter(season == y, Pos %in% c("QB","RB","WR","TE"), Player %in% player.pool$Player) %>% 
-    select(Player, Pos, Tm, week, HalfPPRFantasyPoints) %>% 
-    pivot_wider(names_from = week, values_from = HalfPPRFantasyPoints, values_fill = 0) %>% 
-    mutate(is.flex = if_else(Pos %in% c("RB","WR","TE"), T, F)) # %>% 
+  points = fp.w %>% filter(season == y, position %in% c("QB","RB","WR","TE"), full_name %in% player.pool$full_name) %>% 
+    select(full_name, position, recent_team, week, half_ppr) %>% 
+    pivot_wider(names_from = week, values_from = half_ppr, values_fill = 0) %>% 
+    mutate(is.flex = if_else(position %in% c("RB","WR","TE"), T, F)) # %>% 
     #replace(is.na(.), 0) %>% 
     #column_to_rownames(var = "Player")
   
-  qb.set = unique(pts$Player[which(pts$Pos=="QB")])
-  rb.set = unique(pts$Player[which(pts$Pos=="RB")])
-  wr.set = unique(pts$Player[which(pts$Pos=="WR")])
-  te.set = unique(pts$Player[which(pts$Pos=="TE")])
+  qb.set = unique(points$full_name[which(points$position=="QB")])
+  rb.set = unique(points$full_name[which(points$position=="RB")])
+  wr.set = unique(points$full_name[which(points$position=="WR")])
+  te.set = unique(points$full_name[which(points$position=="TE")])
   
-  qb = which(row.names(points) %in% qb.set)
-  rb = which(row.names(points) %in% rb.set)
-  wr = which(row.names(points) %in% wr.set)
-  te = which(row.names(points) %in% te.set)
-  #dst = which(pts$Pos=="DST")
-  #k = which(pts$Pos=="K")
+  qb = which(points$position=="QB") #which(row.names(points) %in% qb.set)
+  rb = which(points$position=="RB") #which(row.names(points) %in% rb.set)
+  wr = which(points$position=="WR") #which(row.names(points) %in% wr.set)
+  te = which(points$position=="TE") #which(row.names(points) %in% te.set)
+  #dst = which(pts$position=="DST")
+  #k = which(pts$position=="K")
   flex = c(rb,wr,te)
   
   n_players = nrow(points)
   n_weeks = ifelse(y < 2021, 17, 18)
   L = 18
   
+  pts = as.matrix(points[,as.character(1:n_weeks)])
+  
   bestball.base = MIPModel() %>%
-    add_variable(player[i], i = 1:n_players, type = "binary") |>
+    add_variable(player[i], i = 1:n_players, type = "binary") %>% 
     add_variable(x[i,j], i = 1:n_players, j = 1:n_weeks, type = "binary") %>%
-    #add_variable(z, lb = 0) %>% 
-    set_objective(sum_over(points[i,j] * x[i,j], i = 1:n_players, j = 1:n_weeks), "max") %>%
-    #add_constraint(sum_expr(cost[i] * player[i], i = 1:n) <= cap) %>%
+    set_objective(sum_over(pts[i,j] * x[i,j], i = 1:n_players, j = 1:n_weeks), "max") %>%
+    ##add_constraint(sum_expr(cost[i] * player[i], i = 1:n) <= cap) %>%
     add_constraint(sum_over(x[i,j], i = qb) == 1, j = 1:n_weeks) %>%
     #add_constraint(sum_expr(player[i], i = dst) <= 1) %>%
     add_constraint(sum_over(x[i,j], i = rb) <= 3, j = 1:n_weeks) %>%
     add_constraint(sum_over(x[i,j], i = wr) <= 4, j = 1:n_weeks) %>%
     add_constraint(sum_over(x[i,j], i = te) <= 2, j = 1:n_weeks) %>%
+    add_constraint(sum_over(x[i,j], i = rb) >= 2, j = 1:n_weeks) %>%
+    add_constraint(sum_over(x[i,j], i = wr) >= 2, j = 1:n_weeks) %>%
+    add_constraint(sum_over(x[i,j], i = te) >= 1, j = 1:n_weeks) %>%
     add_constraint(sum_over(x[i,j], i = flex) == 7, j = 1:n_weeks) %>% 
     #add_constraint(sum_over(x[i,j], j = 1:n_weeks) <= 17*player[i], i = 1:n_players) %>% 
     add_constraint(x[i,j] <= player[i], i = 1:n_players, j = 1:n_weeks) %>% 
     add_constraint(sum_over(player[i], i = 1:n_players) == L)
   
   draftees.sol = solve_model(bestball.base, with_ROI(solver = "gurobi", verbose = F))
-  #weekly = draftees.sol %>% get_solution(x[i,j]) %>%  filter(value > 0) 
+  weekly = draftees.sol %>% get_solution(x[i,j]) %>%  filter(value > 0) 
   players.sol = draftees.sol %>% get_solution(player[i]) %>%  filter(value > 0) 
   
-  tmp = unique(merge(data.frame(Player = row.names(points[players.sol$i,])), pts[,c("Player","Pos")]))
+  #tmp = unique(merge(data.frame(Player = row.names(points[players.sol$i,])), pts[,c("Player","position")]))
+  tmp = unique(points[players.sol$i,c("full_name", "position", "recent_team")])
   tmp$Year = y
-  players.l[[as.character(y)]] = tmp
+  bestball.l[[as.character(y)]] = tmp
 }
-players = do.call(rbind,players.l)
-table(players[c("Pos","Year")])
+bestball = do.call(rbind,bestball.l)
+table(bestball[c("position","Year")])
